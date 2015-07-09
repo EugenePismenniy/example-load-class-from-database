6db1be1cf29265ad8381d75f22a005b0ae789009
eugene <evgeniy.pismenny@gmail.com>
2015-07-09 13:38:34 +0300

    add decompiling class data

--- src/main/java/com/ukreugene/dbclassloader/Main.java
+++ src/main/java/com/ukreugene/dbclassloader/Main.java
@@ -5,6 +5,10 @@ import java.net.URL;
 import org.apache.commons.io.IOUtils;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import com.strobel.assembler.metadata.ArrayTypeLoader;
+import com.strobel.decompiler.Decompiler;
+import com.strobel.decompiler.DecompilerSettings;
+import com.strobel.decompiler.PlainTextOutput;
 import com.ukreugene.dbclassloader.domain.JavaClass;
 
 public class Main {
@@ -13,8 +17,8 @@ public class Main {
 
 	public static void main(String[] args) throws Exception {
 		//System.setProperty("spring.profiles.active", "hsql");
-		//System.setProperty("spring.profiles.active", "hibernate");
-		System.setProperty("spring.profiles.active", "sybase_hibernate");
+		System.setProperty("spring.profiles.active", "hibernate");
+		// System.setProperty("spring.profiles.active", "sybase_hibernate");
 		
 		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext("com.ukreugene.dbclassloader");
 		
@@ -37,15 +41,34 @@ public class Main {
 		
 		if (restoreClass != null && restoreClass.getDataClass() != null && restoreClass.getDataClass().length > 0) {
 		
-			byte[] dataClass = restoreClass.getDataClass();
-			System.out.println(dataClass.length);
+			final byte[] dataClass = restoreClass.getDataClass();
+			System.out.println(dataClass.length);			
 			
+			// ---------------------- load class to JVM
 			CustomByteArrayClassLoader classLoader = context.getBean(CustomByteArrayClassLoader.class);
 			
 			Class<?> loadedClass = classLoader.loadClass(dataClass);
 			
+			
+			String className = null;
+			
 			if (loadedClass != null) {
-				System.out.println("Class has been loaded: '" + loadedClass.getName() + "'");
+				className = loadedClass.getName();
+				System.out.println("Class has been loaded: '" + className + "'");
+			}
+			
+			// -------------------- decompile byte code
+			if (className != null) {			
+				 
+				DecompilerSettings settings = DecompilerSettings.javaDefaults();
+				settings.setTypeLoader(new ArrayTypeLoader(dataClass));
+				
+				PlainTextOutput output = new PlainTextOutput();
+				
+				//Decompiler.decompile("org/apache/commons/lang3/StringUtils", output, settings);
+				Decompiler.decompile(className.replace('.', '/'), output, settings);
+				
+				System.out.println(output.toString());
 			}
 		}
 	}
